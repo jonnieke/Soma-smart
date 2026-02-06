8b4dedc2e00b3edde9757c27fc33678115125ca7
Peterson <kariukinjoroge13@gmail.com>
2026-02-06 15:17:20 +0300

    Hotfix: Wrap App in Router to resolve useNavigate error

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
@@ -11,6 +12,8 @@ if (!rootElement) {
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );
